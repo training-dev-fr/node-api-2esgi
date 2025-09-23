268c55517695a648c0f06aaea924d8b8048da6d3
training-dev <contact@training-dev.fr>
2025-09-23 12:20:22 +0200

    crud product

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,7 +1,74 @@
-let getAll = () => {
+const Product = require('./../model/product.model');
 
+
+exports.getAll = async (req, res) => {
+    try {
+        let productList = await Product.findAll();
+        res.status(200).json(productList);
+    } catch (e) {
+        res.status(400).json({ error: "Impossible de récupérer les produits" })
+    }
 }
 
-exports.create = () => {
-
+exports.getById = async (req, res) => {
+    try {
+        let produit = await Product.findOne({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json(produit);
+    } catch (e) {
+        res.status(400).json({ error: "Impossible de récupérer les produits" })
+    }
 }
+
+exports.create = async (req, res, next) => {
+    try {
+        let product = await Product.create({
+            name: req.body.name,
+            description: req.body.description,
+            price: req.body.price
+        });
+        res.status(201).json(product);
+    } catch (e) {
+        res.status(400).json({ error: "Impossible de créer le produit!" })
+    }
+}
+
+exports.update = async (req, res, next) => {
+    try {
+        let product = await Product.findOne({
+            where: {
+                id: req.params.id
+            }
+        });
+        if(req.body.name){
+            product.name = req.body.name;
+        }
+        if(req.body.description){
+            product.description = req.body.description;
+        }
+        if(req.body.price){
+            product.price = req.body.price;
+        }
+        product.save();
+        res.status(201).json(product);
+    } catch (e) {
+        res.status(400).json({ error: "Impossible de modifier ce produit" })
+    }
+}
+
+exports.delete = async (req, res) => {
+    try {
+        let product = await Product.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json(product);
+    } catch (e) {
+        res.status(400).json({ error: "Impossible de supprimer ce produit" })
+    }
+}
+
--- route/product.route.js
+++ route/product.route.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const productController = require('./../controller/product.controller');
 
-router.get('/',() => {
-    console.log("Récupération produit");
-});
-router.post('/',() => {
-    console.log("Création produit");
-});
-router.put('/',() => {
-    console.log("Modification produit");
-});
-router.delete('/',() => {
-    console.log("Suppression produit");
-});
+router.get('/',productController.getAll);
+router.get('/:id',productController.getById);
+router.post('/',productController.create);
+router.put('/:id',productController.update);
+router.delete('/:id',productController.delete);
 
 module.exports = router;
